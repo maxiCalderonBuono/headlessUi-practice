7c3a8a95ef23407a04c14d095984158f23da4b15
Maxi <calderonmaxi@outlook.com>
2022-04-02 09:57:47 -0300

    test20

--- src/components/organisms/auth/LoginScreen.js
+++ src/components/organisms/auth/LoginScreen.js
@@ -40,7 +40,7 @@ const LoginScreen = () => {
       <Transition show={isOpen} as={Fragment}>
         <Dialog
           as="div"
-          className="fixed inset-0 z-10 flex flex-row items-center justify-center h-full overflow-y-auto bg-white"
+          className="fixed inset-0 z-10 flex flex-row items-center justify-center h-full overflow-y-auto"
           onClose={() => setIsOpen(false)}
           initialFocus={firstInput}
         >
